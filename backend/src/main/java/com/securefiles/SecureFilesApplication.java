6f1bae94e3752628d444c7a7c9cb36763f128487
Barbas Guillaume <guillaumebarbas@hotmail.fr>
2026-09-15 01:40:53 +0200

    feat(backend): bootstrap spring application

---
+++ backend/src/main/java/com/securefiles/SecureFilesApplication.java
@@ -0,0 +1,12 @@
+package com.securefiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureFilesApplication.class, args);
+    }
+}
